9a794485d506a409d438fa124ae982de1edb3da5
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2019-09-14 19:35:55 +0200

    [add] Placeholder for main scene

--- nodes/Grid/Grid.gd
+++ nodes/Grid/Grid.gd
@@ -1,20 +1,29 @@
 tool
-extends Node2D
+extends Control
 
-export (int, 4, 100) var size = 15 setget set_size
+export (int, 4, 100) var grid_size = 15 setget set_grid_size
 export (bool) var show_grid = true setget set_visibility
 
-func set_size(new_size):
+func set_grid_size(new_grid_size):
 	if !has_node("Lines"):
 		return
 		
-	size = new_size
+	grid_size = new_grid_size
+	
+	var tileMap_size = grid_size * $Lines.cell_size
+	var center_position = Vector2(
+		rect_size.x / 2 - tileMap_size.x / 2,
+		rect_size.y / 2 - tileMap_size.y / 2
+	)
+	
+	$Lines.position = center_position
+	$Content.position = center_position
 	
 	$Lines.clear()
 	$Content.clear()
 
-	for x in range(size):
-		for y in range(size):
+	for x in range(grid_size):
+		for y in range(grid_size):
 			$Lines.set_cell(x, y, 0)
 
 func set_visibility(new_value):
